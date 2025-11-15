b67556e2cb74c5f14ccc343b1303adae3f129f0d
Shivansh Gupta <shivanshg02@gmail.com>
2025-11-14 22:42:48 -0800

    adding sleep program

--- Makefile
+++ Makefile
@@ -173,6 +173,7 @@ mkfs/mkfs: mkfs/mkfs.c $K/fs.h $K/param.h
 
 UPROGS=\
 	$U/_cat\
+	$U/_sleep\
 	$U/_echo\
 	$U/_forktest\
 	$U/_grep\
--- user/sleep.c
+++ user/sleep.c
@@ -1,9 +1,14 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
 int main(int argc, char *argv[]) {
-    if (argc!=1) {
-        sleep(i
-    
-    }
+	if (argc != 2) {
+		printf("Invalid number of arguments \n");
+	}
 
-    exit(0);
-
+	char* sleep_time_str = argv[1];
+	int sleep_time = atoi(sleep_time_str);
+	sleep(sleep_time);
+	exit(0);
 }
